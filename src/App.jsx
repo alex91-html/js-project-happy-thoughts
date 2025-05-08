26c42aee66a8099c0fbd8b530e41d8395771a9aa
alex91-html <alex91-html@users.noreply.github.com>
2025-05-08 13:06:11 +0200

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ThoughtInput from "./components/ThoughtInput";
 import ThoughtList from "./components/ThoughtList";
 
