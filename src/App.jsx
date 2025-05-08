f1d59fef28eea8f6f8bc59ee39106c4d65dfb9fb
alex91-html <alex91-html@users.noreply.github.com>
2025-05-08 13:03:53 +0200

    api url

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,12 @@ import { useState } from "react";
 import ThoughtInput from "./components/ThoughtInput";
 import ThoughtList from "./components/ThoughtList";
 
+const API_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts";
+
 export const App = () => {
   const [thoughts, setThoughts] = useState([]);
 
-  const addThought = (text) => {
-    setThoughts([
-      { text, likes: 0, time: "just now" },
-      ...thoughts,
-    ]);
-  };
 
-  const likeThought = (idx) => {
-    setThoughts(thoughts.map((t, i) =>
-      i === idx ? { ...t, likes: t.likes + 1 } : t
-    ));
-  };
 
   return (
     <div className="min-h-screen bg-gray-50 py-8 flex flex-col items-center px-2">
